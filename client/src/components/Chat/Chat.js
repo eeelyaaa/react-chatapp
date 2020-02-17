1b9c1777783175b086d06cfc970bcc210f13ccb8
Elya <elyamurza@yahoo.com>
2020-02-16 17:49:51 -0800

    final changes for deploying

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -19,7 +19,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState('');
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://socket-react-chatapp.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
